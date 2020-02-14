8c0752498387f3bb195ba061d584401f18b1ac7c
Kautenja <kautencreations@gmail.com>
2020-02-13 23:22:56 -0600

    update

--- __main__.py
+++ __main__.py
@@ -1,2 +1,18 @@
-from splatter.app import APP
-APP.run(port=8080)
+"""The Splatter web server."""
+import argparse
+from splatter.app import app
+
+
+# parse command line arguments
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('--port', '-p',
+    type=int,
+    help='The port to run the service at.',
+    required=False,
+    default=8080
+)
+args = parser.parse_args()
+
+
+# run the application
+app.run(port=args.port)
--- splatter/app.py
+++ splatter/app.py
@@ -1,32 +1,37 @@
 """The splatter web application"""
 import os
 import flask
+import phue
 
 
 # create the Flask web server
-APP = flask.Flask(__name__)
+app = flask.Flask(__name__)
+
+# create the connection to the Hue bridge
+bridge = phue.Bridge()
 
 
-@APP.route('/site.webmanifest')
+@app.route('/site.webmanifest')
 def webmanifest():
     """Return the web manifest associated with the application."""
     return flask.send_from_directory(
-        os.path.join(APP.root_path, 'static'),
+        os.path.join(app.root_path, 'static'),
         'site.webmanifest', mimetype='application/manifest+json'
     )
 
 
-@APP.route('/favicon.ico')
+@app.route('/favicon.ico')
 def favicon():
     """Return the favicon associated with the application."""
     return flask.send_from_directory(
-        os.path.join(APP.root_path, 'static'),
+        os.path.join(app.root_path, 'static'),
         'favicon.ico', mimetype='image/vnd.microsoft.icon'
     )
 
 
 # create the route for the root URL
-@APP.route("/")
+@app.route("/")
 def home():
     """Return the home page."""
+    print(bridge.ip)
     return flask.render_template("home.html")
