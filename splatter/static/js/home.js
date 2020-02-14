1bf7d789b012078046e77089a8c7e2fee4a6aab9
Kautenja <kautencreations@gmail.com>
2020-02-14 06:03:38 -0600

    cleanup

--- splatter/static/js/home.js
+++ splatter/static/js/home.js
@@ -1,22 +1,13 @@
 
-// function set_brightness(light_id, value) {
-//     $.ajax({
-//         type: "POST",
-//         contentType: "application/json; charset=utf-8",
-//         url: "/lights",
-//         data: JSON.stringify({
-//             "light_id": light_id,
-//             "function": "bri",
-//             "value": value
-//         }),
-//         success: function (data) {
-//             console.log("TODO: giggity");
-//         },
-//         dataType: "json"
-//     });
-// }
+/**
+    Set a value for a light on the hue server.
 
-function set_hue_value(light_id, parameter, value) {
+    @param light_id the ID for the light to set
+    @param parameter the name of the parameter to set
+    @param value the value to set the parameter to
+
+*/
+function set_light(light_id, parameter, value) {
     $.ajax({
         type: "POST",
         contentType: "application/json; charset=utf-8",
@@ -32,8 +23,3 @@ function set_hue_value(light_id, parameter, value) {
         dataType: "json"
     });
 }
-
-function set_brightness(light_id, value) {
-    set_hue_value(light_id, "bri", value);
-}
-
